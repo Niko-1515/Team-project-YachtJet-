9752c6034902c7fe7e4f92fb1028dd952413722a
KaterynaNat <kate.naturkach@gmail.com>
2024-08-11 19:22:59 +0300

    fix script

--- src/js/menu.js
+++ src/js/menu.js
@@ -3,45 +3,37 @@
   const openMenuBtn = document.querySelector('.js-open-menu');
   const closeMenuBtn = document.querySelector('.js-close-menu');
 
-  const toggleMenu = () => {
-    const anchors = mobileMenu.querySelectorAll('a[href*="#"]');
+  // добавили доп класс что бы закрыть после нажатия на ссылку
+  const menuLinks = document.querySelectorAll('.mobile-modal-link');
+const toggleMenu = () => {
     const isMenuOpen =
       openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
     openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
     mobileMenu.classList.toggle('is-open');
-    
 
     const scrollLockMethod = !isMenuOpen
       ? 'disableBodyScroll'
       : 'enableBodyScroll';
     bodyScrollLock[scrollLockMethod](document.body);
-    
-    if(anchors.length === 0) return;
-    
-    if(!isMenuOpen) {
-      anchors.forEach(anchor => {
-        anchor.addEventListener("click", toggleMenu)
-      })
-      return;
-    }
-    
-    anchors.forEach(anchor => {
-        anchor.removeEventListener("click", toggleMenu)
-      })
   };
 
+//  добавили что бы закрывалось при нажатии на ссылку
+  menuLinks.forEach(menuLink => {
+    menuLink.addEventListener('click', toggleMenu);
+  });
+
+  // стандартно из скрипта от Репеты
   openMenuBtn.addEventListener('click', toggleMenu);
   closeMenuBtn.addEventListener('click', toggleMenu);
 
-  // Вказати брейкпоінт після якого повинна зачинятися
-  window.matchMedia('(min-width: 375px)').addEventListener('change', e => {
+// Close the mobile menu on wider screens if the device orientation changes
+  window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
     if (!e.matches) return;
     mobileMenu.classList.remove('is-open');
     openMenuBtn.setAttribute('aria-expanded', false);
     bodyScrollLock.enableBodyScroll(document.body);
   });
 })();
-
 
 //Menu open-close animation//
 (() => {
