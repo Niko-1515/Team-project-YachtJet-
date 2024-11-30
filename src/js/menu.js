6c56a63a052452a76412fe3b8f29d3500efa3fb7
Niko <nikodimstroy@gmail.com>
2024-11-30 20:25:12 +0100

    fix preloader

--- src/js/menu.js
+++ src/js/menu.js
@@ -30,3 +30,24 @@
     bodyScrollLock.enableBodyScroll(document.body);
   });
 })();
+
+
+
+//  preloader
+let hellopreloader = document.querySelector('.preloader');
+function fadeOutnojquery(el) {
+  el.style.opacity = 1;
+  let interhellopreloader = setInterval(function () {
+    el.style.opacity = el.style.opacity - 0.05;
+    if (el.style.opacity <= 0.05) {
+      clearInterval(interhellopreloader);
+      hellopreloader.style.display = 'none';
+    }
+  }, 20);
+}
+
+window.onload = function () {
+  setTimeout(function () {
+    fadeOutnojquery(hellopreloader);
+  }, 2500);
+};
