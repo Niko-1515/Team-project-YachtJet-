7ce1861328d797a0a93c7668344a8abcb53bb330
Niko <nikodimstroy@gmail.com>
2024-12-02 22:39:07 +0100

    set js

--- src/main.js
+++ src/main.js
@@ -1,1 +1,21 @@
 import './js/menu.js';
+
+// preloader
+
+let hellopreloader = document.querySelector('.preloader');
+function fadeOutnojquery(el) {
+  el.style.opacity = 1;
+  let interhellopreloader = setInterval(function () {
+    el.style.opacity = el.style.opacity - 0.05;
+    if (el.style.opacity <= 0.05) {
+      clearInterval(interhellopreloader);
+      hellopreloader.style.display = 'none';
+    }
+  }, 20);
+}
+
+window.onload = function () {
+  setTimeout(function () {
+    fadeOutnojquery(hellopreloader);
+  }, 2500);
+};
